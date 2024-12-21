6d7864910a06f7ed824c9b91e2a2f379887c36f4
Alfaidar16 <alfaidaramirjr@gmail.com>
2024-12-22 00:34:36 +0800

    login

--- src/components/views/auth/login/index.tsx
+++ src/components/views/auth/login/index.tsx
@@ -1,8 +1,77 @@
+import Link from "next/link";
+import Styles from "./Login.module.scss";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
+
+import { signIn } from "next-auth/react";
+
 const LoginView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { push, query } = useRouter();
+  const callbackUrl: any = query.callbackUrl || "/";
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError("");
+    const form = event.target as HTMLFormElement;
+    try {
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: form.email.value,
+        password: form.password.value,
+        callbackUrl: callbackUrl,
+      });
+
+      if (!res?.error) {
+        setIsLoading(false);
+        form.reset();
+        push(callbackUrl);
+      } else {
+        setIsLoading(false);
+        setError("Email Atau Password Salah");
+        form.reset();
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setError("Terjadi Kesalahan Pada Server");
+    }
+  };
   return (
-    <div>
-      <h1>Login View</h1>
+    <div className={Styles.login}>
+      <h1 className={Styles.login__title}>Login</h1>
+      {error && <p className={Styles.login__error}>{error}</p>}
+      <div className={Styles.login__form}>
+        <form onSubmit={handleSubmit}>
+          <div className={Styles.login__form__item}>
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className={Styles.login__form__item__input}
+            />
+          </div>
+
+          <div className={Styles.login__form__item}>
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className={Styles.login__form__item__input}
+            />
+          </div>
+          <button type="submit" className={Styles.login__form__button}>
+            {isLoading ? "Loading" : " login"}
+          </button>
+        </form>
+      </div>
+      <p className={Styles.login__link}>
+        Belum Punya Akun ? <Link href="/auth/register">Daftar</Link>
+      </p>
     </div>
   );
 };
+
 export default LoginView;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import Navbar from "@/components/layouts/navbar";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Manrope } from "next/font/google";
 
@@ -7,10 +9,16 @@ const manrope = Manrope({
   weight: ["400", "700", "200", "300"],
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, pageProps },
+}: AppProps) {
   return (
-    <div className={manrope.className}>
-      <Component {...pageProps} />;
-    </div>
+    <SessionProvider session={session}>
+      <div className={manrope.className}>
+        <Navbar />
+        <Component {...pageProps} />;
+      </div>
+    </SessionProvider>
   );
 }
